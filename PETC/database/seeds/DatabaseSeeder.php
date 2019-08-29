45cb56e6e6eb048872fdbfab75263f7df5cdb429
luisviramontes <luis_alfonso133@hotmail.com.com>
2019-08-29 12:06:57 -0500

    migraciones

--- PETC/database/seeds/DatabaseSeeder.php
+++ PETC/database/seeds/DatabaseSeeder.php
@@ -25,34 +25,14 @@ class DatabaseSeeder extends Seeder
         $this->call(CatPuestoSeeder::class);
         $this->call(MunicipiosSeeder::class);
         $this->call(LocalidadesSeeder::class);
-<<<<<<< HEAD
-        
         $this->call(CentrosTrabajoSeeder::class);
-        
-=======
-
-
-        $this->call(CentrosTrabajoSeeder::class);
-
-
-
-        $this->call(CentrosTrabajoSeeder::class);
-
-
->>>>>>> a18c39caa8bcb5162e50c0e016c19633faf2ea73
         $this->call(DatosCentroTrabajoSeeder::class);
         $this->call(dia_mes_seeder::class);
-
         $this->call(TablaPagosSeeder::class);
         $this->call(CapturaSeeder::class);
         $this->call(Captura2Seeder::class);
-        
         $this->call(Director_CCTSeeder::class);
-
-
-
         $this->call(DirectorioSeeder::class);
-
         $this->call(DirectorioInternoSeeder::class);
         $this->call(Plan_Contraste_NominaSeeder::class);
 
